10456efa333a1277cfcba5342ff65e34ee79ba36
Michael Smyers <msmyers@zipwhip.com>
2013-05-10 14:09:50 -0700

    Initial import of project.

---
+++ src/test/java/com/zipwhip/MockTest.java
@@ -0,0 +1,19 @@
+package com.zipwhip;
+
+import org.junit.Test;
+
+/**
+ * Date: 5/10/13
+ * Time: 2:08 PM
+ *
+ * @author Michael
+ * @version 1
+ */
+public class MockTest {
+
+    @Test
+    public void testIt() {
+        // this is an empty unit test.
+    }
+
+}
